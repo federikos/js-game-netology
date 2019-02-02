d82c0a516d340888b8421ed6c4ed9f0cc0800a13
Fiodar Savutsin <federikos@bk.ru>
2019-02-03 00:45:21 +0300

    implemented class Vector with methods

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
 'use strict';
+
+class Vector {
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
+	plus(vector) {
+		if(!(vector instanceof Vector)) {
+			throw new Error('Ошибка');
+		}
+		return new Vector(vector.x + this.x, vector.y + this.y);
+	}
+	times(n) {
+		return new Vector(this.x * n, this.y * n);
+	}
+}
