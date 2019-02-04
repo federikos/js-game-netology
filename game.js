9b402b2c25b1477f58cdd69c388e763e67d9320d
Fiodar Savutsin <federikos@bk.ru>
2019-02-04 22:47:51 +0300

    Classes Vector, Actor passed tests.

--- game.js
+++ game.js
@@ -5,40 +5,39 @@ class Vector {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	plus(vector) {
 		if(!(vector instanceof Vector)) {
 			throw new Error('Ошибка');
 		}
 		return new Vector(vector.x + this.x, vector.y + this.y);
 	}
-	
+
 	times(n) {
 		return new Vector(this.x * n, this.y * n);
 	}
 }
 
 class Actor {
-	constructor(position, size, speed) {
-		this.pos = new Vector();
-		this.size = new Vector(1, 1);
-		this.speed = new Vector();
-		if(position instanceof Vector) {
+	constructor(position = new Vector(),
+			    size = new Vector(1, 1),
+			    speed = new Vector()) {
+		if(position instanceof Vector &&
+		   size instanceof Vector &&
+		   speed instanceof Vector) {
 			this.pos = position;
-		 }
-		if(size instanceof Vector) {
 			this.size = size;
+			this.speed = speed;
+		} else {
+			throw new Error('В конструктор передан не вектор');
 		}
-		// if (!(position instanceof Vector)) {
-		// 	throw new Error('Ошибка');
-		// }
 		this.left = this.pos.x;
 		this.right = this.pos.x + this.size.x;
 		this.top = this.pos.y;
 		this.bottom = this.pos.y + this.size.y;
 
 	}
-	
+
 	act() {
 	}
 	isIntersect(anyObj) {
@@ -71,7 +70,7 @@ class Actor {
 		}
 
 		//Объект не пересекается с объектом расположенным в той же точке, но имеющим отрицательный вектор размера
-		if(anyObj.pos.x === this.pos.x && 
+		if(anyObj.pos.x === this.pos.x &&
 			anyObj.pos.y === this.pos.y &&
 			anyObj.size.x <= 0 &&
 			anyObj.size.y <= 0) {
@@ -92,3 +91,44 @@ class Actor {
 Object.defineProperty(Actor.prototype, 'type', {
   	value: 'actor'
 })
+
+class Level {
+	constructor(grid = [], actors = []) {
+		this.grid = grid;
+		this.actors = actors;
+		this.height = grid.length;
+		this.width = 0;
+
+		let diff = false;
+		if(grid[0]) {
+			let max = grid[0];
+			for(let i = 0; i < grid.length; i++) {
+				if(grid[i].length > max) {
+					max = grid[i].length;
+					diff = true;
+				}
+			}
+			this.width = diff ? grid[max].length : grid[0].length;
+		}
+		this.status = null;
+		this.finishDelay = 1;
+		this.player = this.actors.filter(actor => actor.type === 'player').shift();
+	}
+	isFinished() {
+		if(this.status !== null && this.finishDelay < 0) {
+			return true;
+		}
+		return false;
+	}
+	actorAt(actor) {
+		if(actor === undefined || !(actor instanceof Actor)) {
+			throw new Error('Ошибка в actor');
+		}
+		for(let i = 0; i < this.actors.length; i++) {
+			let obj = this.actors[i];
+			if(actor.isIntersect(obj)) {
+				return obj;
+			}
+		}
+	}
+}
