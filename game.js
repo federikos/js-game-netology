28895476d8a89650d5353aba551762f310d3bb59
Fiodar Savutsin <federikos@bk.ru>
2019-02-07 21:28:56 +0300

    method isIntersect fixed

--- game.js
+++ game.js
@@ -54,50 +54,22 @@ class Actor {
 	}
 
 	isIntersect(anyObj) {
-		const farAwayDistance = 100;
 		if(!(anyObj instanceof Actor)) {
-			throw new Error('Объект не является экземпляром Actor');
+		 	throw new Error('Объект не является экземпляром Actor');
 		}
-		if(anyObj === this) {
-			return false;
-		}
-		if(
-			(Math.abs(anyObj.pos.x - this.pos.x) >= farAwayDistance) ||
-		    (Math.abs(anyObj.pos.y - this.pos.y) >= farAwayDistance)
-		  ) {
+
+		if(this === anyObj) {
 			return false;
 		}
 
-		//Объект не пересекается с объектом со смежными границами
-		if(anyObj.pos.x === this.pos.x + this.size.x) {
-			return false;
-		}
-		if(anyObj.pos.y === this.pos.y + this.size.y) {
-			return false;
-		}
-		if(this.pos.x === anyObj.pos.x + anyObj.size.x) {
-			return false;
-		}
-		if(this.pos.y === anyObj.pos.y + anyObj.size.y) {
-			return false;
-		}
-
-		//Объект не пересекается с объектом расположенным в той же точке, но имеющим отрицательный вектор размера
-		if(anyObj.pos.x === this.pos.x &&
-			anyObj.pos.y === this.pos.y &&
-			anyObj.size.x <= 0 &&
-			anyObj.size.y <= 0) {
-			return false;
-		}
-		//Объект пересекается с объектом, который полностью содержится в нём
-		//Объект пересекается с объектом, который частично содержится в нём
-		if(anyObj.pos.x > this.pos.x ||
-			anyObj.pos.y > this.pos.y ||
-			anyObj.pos.x + anyObj.size.x < this.pos.x + this.size.x ||
-			anyObj.pos.y + anyObj.size.y < this.pos.y + this.size.y
-			) {
+		if(this.right > anyObj.left &&
+			this.left < anyObj.right &&
+			this.top < anyObj.bottom &&
+			this.bottom > anyObj.top) {
 			return true;
 		}
+		
+		return false;
 	}
 }
 
